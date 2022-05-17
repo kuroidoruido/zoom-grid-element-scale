e1609287f8d9f747bb697be6d6dca67f01da14cf
kuroidoruido <anthony.pena@outlook.fr>
2022-05-18 00:06:59 +0200

    3 files modified, 1 added, 1 deleted

--- main.js
+++ main.js
@@ -1,29 +1,18 @@
-import './style.css';
+import './style.scss';
 
 document.querySelector('#app').innerHTML = `
-<div class="container">
-  <div class="element">
-    <div class="wrapper">Element</div>
-  </div>
-  <div class="element x2">
-    <div class="wrapper">Element X2</div>
-  </div>
-  <div class="element x3">
-    <div class="wrapper">Element X3</div>
-  </div>
-</div>
-
+<!-- ZOOM LEVEL SELECTOR -->
 <form>
   <ul>
     <li>
       <label for="small">
-        <input type="radio" name="zoom" value="small" id="small" />
+        <input type="radio" name="zoom" value="small" id="small" checked />
         Small
       </label>
     </li>
     <li>
       <label for="medium">
-        <input type="radio" name="zoom" value="medium" id="medium" checked />
+        <input type="radio" name="zoom" value="medium" id="medium" />
         Medium
       </label>
     </li>
@@ -35,4 +24,40 @@ document.querySelector('#app').innerHTML = `
     </li>
   </ul>
 </form>
+
+<!-- ZOOMED ELEMENTS -->
+
+<div id="container" class="zoom-small">
+  ${range(1, 10)
+    .map(
+      (elt) => `
+    <div class="element">
+      <div class="content">
+        Element ${elt}
+        <button type="button">Click on the 👻</button>
+      </div>
+    </div>`
+    )
+    .join('')}
+</div>
 `;
+
+function range(from, to) {
+  return new Array(1 + to - from).fill(null).map((_, elt) => from + elt);
+}
+
+function onZoomClick(size) {
+  return () => {
+    document.getElementById('container').className = 'zoom-' + size;
+  };
+}
+
+document
+  .getElementById('small')
+  .addEventListener('click', onZoomClick('small'));
+document
+  .getElementById('medium')
+  .addEventListener('click', onZoomClick('medium'));
+document
+  .getElementById('large')
+  .addEventListener('click', onZoomClick('large'));
